d56f70ea4eb35d13472c0105a71779ed9f3d9052
Tariq Zaghal <zaghaltariq007@gmail.com>
2023-11-11 02:05:49 +0200

    ADD: Add indexes to the prominent columns in each of the three tables

---
+++ indexes/CreateBookIndexes.sql
@@ -0,0 +1,11 @@
+CREATE INDEX Book_Title_Index
+on Book(Title);
+
+CREATE INDEX Book_Author_Index
+on Book(Author);
+
+CREATE INDEX Book_Genre_Index
+on Book(Genre);
+
+CREATE INDEX Book_Status_Index
+on Book(CurrentStatus);
